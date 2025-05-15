49096a904b09532ca694b10d7aa8d7d7c15c2624
scr1pticle <erpoluden@gmail.com>
2025-05-15 07:47:28 +0200

    program cs updated

--- api/Program.cs
+++ api/Program.cs
@@ -1,12 +1,14 @@
 using Scalar.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using api.Models;
+using api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddScoped<IGroupService, GroupService>();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<SplitterContext>(opt =>
     opt.UseInMemoryDatabase("Splitter"));
