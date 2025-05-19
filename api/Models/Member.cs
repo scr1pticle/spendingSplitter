091dd85dd63522417d93b1615389bd4f882860b3
scr1pticle <erpoluden@gmail.com>
2025-05-19 03:15:10 +0200

    member model update

--- api/Models/Member.cs
+++ api/Models/Member.cs
@@ -6,5 +6,8 @@ namespace api.Models
         public string Name { get; set;} = null!;
         public int GroupId { get; set; }
         public Group Group { get; set; } = null!;
+        public ICollection<Share> Shares { get; set; } = new List<Share>();
+        public ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+        public bool IsSelf { get; set; }
     }
 }
